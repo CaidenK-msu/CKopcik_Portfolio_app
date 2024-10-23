a4c74d533d589473eb2f3b5496633190c1ce9b2b
CaidenK-msu <ckopcik@msudenver.edu>
2024-10-23 14:10:15 -0600

    importing devise and did db migrate Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_one_attached :profile_picture
 
     validates :first_name, :last_name, :major, :graduation_date, presence:true
