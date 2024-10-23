602176f365d78dadac5efd7c9c9698e4f14c968f
CaidenK-msu <ckopcik@msudenver.edu>
2024-10-23 13:53:22 -0600

    removed Email message

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ApplicationRecord
     has_one_attached :profile_picture
 
-    validates :first_name, :last_name, :school_email, :major, :graduation_date, presence:true
-    validates :school_email , uniqueness: {message:"Email Already exists"}
-    validates :school_email, format: { with: /\A[\w+\-.]+@msudenver\.edu\z/i, message: "must be an @msudenver.edu email address" }
+    validates :first_name, :last_name, :major, :graduation_date, presence:true
+'''    validates :school_email , uniqueness: {message:"Email Already exists"}
+    validates :school_email, format: { with: /\A[\w+\-.]+@msudenver\.edu\z/i, message: "must be an @msudenver.edu email address" }'''
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_04_204835) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_23_193747) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,8 +39,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_04_204835) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "students", force: :cascade do |t|
-    t.string "school_email"
     t.string "major"
     t.string "minor"
     t.date "graduation_date"
@@ -48,6 +59,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_04_204835) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
